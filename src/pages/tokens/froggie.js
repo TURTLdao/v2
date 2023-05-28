c2664d480bf04123434e44b8e457ba0ff928bd00
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-28 21:57:54 +0100

    Apply improvements to FROGGIE

--- src/pages/tokens/froggie.js
+++ src/pages/tokens/froggie.js
@@ -1,25 +1,20 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { Box, Card, CardContent, Container, Divider, Unstable_Grid2 as Grid } from '@mui/material';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { useEffect, useState } from 'react';
 
-//import { AdaPriceCard } from 'src/sections/dashboard/price-cards/ada';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { FroggieNFTs } from 'src/sections/launchpad/showcase/froggie-nfts';
+import { BuyFroggie } from 'src/sections/launchpad/buy/froggie';
+import { AdaCompareCard } from 'src/sections/launchpad/market-cards/compare';
+import { PriceCard } from 'src/sections/launchpad/market-cards/price';
+
 import { FroggiePie } from 'src/sections/launchpad/charts/distribution';
 import { FroggieEvents } from 'src/sections/launchpad/events/froggie';
 import { FroggieProfileCard } from 'src/sections/launchpad/profiles/froggie';
 import { FroggieBio } from 'src/sections/launchpad/bios/froggie';
 
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { border } from '@mui/system';
-import { FroggieNFTs } from 'src/sections/launchpad/showcase/froggie-nfts';
-import { FroggiePriceChart } from 'src/sections/launchpad/charts/froggie-price';
-
-import { FroggieAdaCompareCard } from 'src/sections/launchpad/cards/froggie/froggie-ada';
-import { FroggieMarketcapCard } from 'src/sections/launchpad/cards/froggie/froggie-mc';
-import { FroggiePooledCard } from 'src/sections/launchpad/cards/froggie/froggie-pooled';
-import { FroggiePriceCard } from 'src/sections/launchpad/cards/froggie/froggie-price';
-import { BuyFroggie } from 'src/sections/launchpad/buy/froggie';
 import { getLastPrice } from 'src/api/fetch-calls';
 
 export async function getStaticProps() {
@@ -30,7 +25,7 @@ export async function getStaticProps() {
 
     return {
       props: {
-        froggie_price: price,
+        froggie_price: price
       },
     };
   } catch (error) {
@@ -38,7 +33,7 @@ export async function getStaticProps() {
 
     return {
       props: {
-        froggie_price: 0,
+        froggie_price: 0
       },
     };
   }
@@ -54,7 +49,17 @@ export default function Page({ froggie_price }) {
     fetchData();
   }, []);
 
+  const ticker = '$FROGGIE'
+  const supply = 69000000000;
+  const u_mc = Number(supply * froggie_price).toFixed(0);
+  const formatter = new Intl.NumberFormat('en-US');
+
+  const formatted_marketcap = formatter.format(u_mc);
+  const marketcap_title = ticker + ' Marketcap'
+
   const formatted_price = Number(froggie_price).toFixed(8);
+  const price_title = ticker + ' Price';
+  const token_logo = '/token-images/froggie.png';
 
   const theme = createTheme({
     palette: {
@@ -87,15 +92,15 @@ export default function Page({ froggie_price }) {
           spacing={3}
         >
           <Grid xs={12} sm={6} lg={3} >
-            <FroggiePriceCard sx={{ height: '100%', marginRight: '10px' }} lastPrice={formatted_price} />
+            <PriceCard sx={{ height: '100%', marginRight: '10px' }} lastPrice={formatted_price} imageLink={token_logo} cardTitle={price_title}/>
           </Grid>
 
           <Grid xs={12} sm={6} lg={3} >
-            <FroggieMarketcapCard sx={{ height: '100%', marginRight: '10px' }} lastPrice={froggie_price} />
+            <PriceCard sx={{ height: '100%', marginRight: '10px' }} lastPrice={formatted_marketcap} cardTitle={marketcap_title}/>
           </Grid>
 
           <Grid xs={12} sm={6} lg={3} >
-            <FroggieAdaCompareCard sx={{ height: '100%', marginRight: '10px' }} />
+            <AdaCompareCard sx={{ height: '100%', marginRight: '10px' }} tokenPrice={froggie_price} ticker={ticker} />
           </Grid>
           
         </Grid>
@@ -123,7 +128,7 @@ export default function Page({ froggie_price }) {
               lg={8}
             >
               <FroggiePie
-                chartSeries={[32.72, 10.89, 20.72, 10.89, 5.46, 19.32]}
+                chartSeries={[43.48, 14.49, 27.54, 14.49, 7.25, 25.75]}
                 labels={['LP', 'Airdrops', 'OTC', 'Distribution', 'More LP', 'Development']}
                 sx={{ height: '100%', marginRight: '10px' }}
               />
