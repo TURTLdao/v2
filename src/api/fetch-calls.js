a6acfe636d6905c11f930fba5fe05c67abda53f4
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-28 13:29:16 +0100

    Woulda been nice to add this before
    Lets download a gui for github

--- src/api/fetch-calls.js
+++ src/api/fetch-calls.js
@@ -1,3 +1,17 @@
+
+export async function getLastPrice(baseId) {
+  const response = await fetch('https://api-mainnet-prod.minswap.org/coinmarketcap/v2/pairs');
+  const data = await response.json();
+
+  const pair = data[baseId];
+
+  if (pair && pair.last_price) {
+    return pair.last_price;
+  }
+
+  return 0; // Return 0 if baseId or last_price is not found
+}
+
 export async function fetchMinswapFroggie() {
   try {
     const response = await fetch('https://api-mainnet-prod.minswap.org/coinmarketcap/v2/pairs');
