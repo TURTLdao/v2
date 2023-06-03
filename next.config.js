7b11e74f78d9133aeff0af25cc3b9e87178a039a
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-06-03 08:00:39 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  generateTimeout: 100000, // Set a higher timeout value (in milliseconds)
+  staticPageGenerationTimeout: 100000, // Set a higher timeout value (in milliseconds)
 };
